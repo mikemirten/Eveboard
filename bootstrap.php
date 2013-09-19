2facedf8fa644c9050b2f957983d597a2f146941
mikemirten <mike@viboom.ru>
2013-09-19 19:29:24 +0400

    Application files added

--- bootstrap.php
+++ bootstrap.php
@@ -1,19 +1,43 @@
 <?php
-use Eveboard\Killmail\Parser;
+use Phalcon\Loader;
+use Phalcon\Mvc\Application;
+use Phalcon\Config\Adapter\Ini;
+use Phalcon\Mvc\View;
+use Phalcon\DI\FactoryDefault as Di;
 
 define('ROOT_PATH', __DIR__);
 
 define('TMP_PATH', ROOT_PATH . DIRECTORY_SEPARATOR . 'tmp');
 define('LIB_PATH', ROOT_PATH . DIRECTORY_SEPARATOR . 'library');
+define('APP_PATH', ROOT_PATH . DIRECTORY_SEPARATOR . 'application');
+define('CONF_PATH', APP_PATH . DIRECTORY_SEPARATOR . 'configs');
 
-$loader = new Phalcon\Loader();
+$loader = new Loader();
 
 $loader->registerNamespaces([
 	'Eveboard' => LIB_PATH . DIRECTORY_SEPARATOR . 'Eveboard'
 ]);
 
+$loader->registerDirs([
+	APP_PATH . DIRECTORY_SEPARATOR . 'controllers'
+]);
+
 $loader->register();
 
-$parser = new Parser(file_get_contents(TMP_PATH . '/killmail1'));
+$di = new Di();
 
-var_dump($parser->toArray());
+$di->set('view', function() {
+	$view = new View();
+	$view->setViewsDir(APP_PATH . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR);
+	$view->setMainView('layout');
+	
+	return $view;
+});
+
+$di->set('config', function() {
+	return new Ini(CONF_PATH . DIRECTORY_SEPARATOR . 'settings.ini');
+});
+
+$application = new Application($di);
+
+echo $application->handle()->getContent();
